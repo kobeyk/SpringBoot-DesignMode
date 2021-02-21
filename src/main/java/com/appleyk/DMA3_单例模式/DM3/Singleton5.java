539ee5b64daa133ac9c54458c26200a0dc3f964f
yukun <437704792@qq.com>
2021-02-21 15:11:06 +0800

    单例增加一个案例变种：DCL单例模式

--- src/main/java/com/appleyk/DMA3_单例模式/DM3/Singleton5.java
+++ src/main/java/com/appleyk/DMA3_单例模式/DM3/Singleton5.java
@@ -1,29 +1,32 @@
 package com.appleyk.DMA3_单例模式.DM3;
 
 /**
- * <p>单例模式 -- 线程安全，只在第一次创建实例的时候加锁</p>
+ * <p>DCL（双重检查锁）单例模式 -- 线程安全，只在第一次创建实例的时候加锁</p>
  *
  * @author Appleyk
  * @blob https://blog.csdn.net/appleyk
  * @date Created on 下午 1:12 2018-11-6
  * @version V.0.1.1
  */
-public class Singleton3 {
+public class Singleton5 {
 
     // 私有构造器，防止外部new实例
-    private Singleton3(){}
+    private Singleton5(){}
 
-    private static Singleton3 instance = null;
+    // 加volatile，防止指令冲排序
+    private static volatile Singleton5 instance = null;
 
     /**
      * 区别于Singleton2,注意synchronized关键字【同步块】的位置
+     * @return
      */
-    public static Singleton3 getInstance() {
+    public static Singleton5 getInstance() {
         if (instance == null) {
-            synchronized (Singleton3.class){
+            synchronized (Singleton5.class){
                 if(instance == null){
-                    instance = new Singleton3();
+                    instance = new Singleton5();
                 }
+                return instance;
             }
         }
         return instance;
